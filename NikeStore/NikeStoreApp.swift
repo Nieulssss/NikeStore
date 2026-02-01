157e84e77bccb1edbc73dab47b030bf6bf6e8878
Niels.Stiefvater <niels.stiefvater@etu.univ-smb.fr>
2026-02-02 00:12:30 +0100

    Initial Commit

---
+++ NikeStore/NikeStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikeStoreApp.swift
+//  NikeStore
+//
+//  Created by Niels on 02/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct NikeStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
